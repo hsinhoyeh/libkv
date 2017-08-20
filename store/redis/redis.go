b78998eb09fa1abfbc91b317ae09dfdc2677af32
hsinhoyeh <yhh92u@gmail.com>
2017-08-20 11:14:01 +0800

    fix import path

--- store/redis/redis.go
+++ store/redis/redis.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/docker/libkv"
-	"github.com/docker/libkv/store"
+	"github.com/hsinhoyeh/libkv"
+	"github.com/hsinhoyeh/libkv/store"
 
 	"gopkg.in/redis.v5"
 )
